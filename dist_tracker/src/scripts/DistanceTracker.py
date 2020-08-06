bf55a5e67a485cbf3a8c8d1d93a847e994b806c9
Braden Cook <bncook@mit.edu>
2020-08-06 14:07:30 -0400

    added rate control of the publishers to the tracking program

--- dist_tracker/src/scripts/DistanceTracker.py
+++ dist_tracker/src/scripts/DistanceTracker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Distance Tracker
-# Given a camera for input, will output the distance between the camera and 
+# Given a camera for input, will output the distance between the camera and
 #   a predetermined (hardcoded) target
 #
 # Author: Braden Cook
@@ -14,13 +14,15 @@ from std_msgs.msg import String, Float64
 from sensor_msgs.msg import Image, CompressedImage
 import cv2
 from cv_bridge import CvBridge
+import numpy as np
 
 class DistanceTracker:
-    """ 
+    """
     Distance Tracker - tracks the distance between a yellow block and a camera
-    using triangle similarity 
+    using triangle similarity
     """
     def __init__(self):
+        self.image = np.zeros((960,1280,3), np.uint8)
         #taget object is yellow
         self.hsv_lower_lower = (14,55,55)
         self.hsv_lower_upper = (30,255,235)
@@ -33,7 +35,7 @@ class DistanceTracker:
         #self.hsv_upper_upper = self.hsv_lower_upper
 
         #parameters
-        self.focal_length = 993.0               #the focal length of the camera 
+        self.focal_length = 993.0               #the focal length of the camera
         self.real_height = 1.25                 #the real height of the target object
         self.image_center = (635.08, 479.80)    #the image center of the camera
         self.rectified = False                  #is the camera using the rectified image
@@ -52,22 +54,22 @@ class DistanceTracker:
         self.current_point = 0
         self.average = 0
 
-    def find_position(self, focal_length, real_height, image):
-        """ 
+    def find_position(self, focal_length, real_height):
+        """
         Finds the distance from the camera to a target of known height
         as well as the target's offset from the camera in the x and y planes
 
         params:
             focal_length (float): the focal length of the camera
-            real_height (float): the real world height of the target object 
+            real_height (float): the real world height of the target object
                  standing in an upright position
             image: the cv2 image from which to seach for the target object
         returns:
-            dist, (x_offset, y_offset) 
+            dist, (x_offset, y_offset)
             the estimated distance between the target object and
             and the camera and the target's offset from image center
         """
-        height_px, offset_px = self.process_image(image)
+        height_px, offset_px = self.process_image(self.image)
         if height_px == 0:
             return 0, (0,0)
         dist = (focal_length * real_height) / height_px
@@ -89,8 +91,8 @@ class DistanceTracker:
         mask_l = cv2.inRange(img_hsv, self.hsv_lower_lower, self.hsv_lower_upper)
         mask_u = cv2.inRange(img_hsv, self.hsv_upper_lower, self.hsv_upper_upper)
         mask = cv2.bitwise_or(mask_l, mask_u)
-        
-        #Publish the mask          
+
+        #Publish the mask
         mask_bgr8 = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
         bridge = CvBridge()
         cv_mask = bridge.cv2_to_imgmsg(mask_bgr8, encoding='bgr8')
@@ -99,16 +101,16 @@ class DistanceTracker:
         #find the largest contour of the mask or return 0 if target is not there
         img, cnts, cnt_hier = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         if len(cnts) == 0:
-            return (0, (0,0))        
+            return (0, (0,0))
         c = max(cnts, key=cv2.contourArea)
 
         #find the height of the target object and its center using minAreaRect
-        rect = cv2.minAreaRect(c)        
+        rect = cv2.minAreaRect(c)
         height_px =  rect[1][1] / self.subsample_ratio
         target_x =   rect[0][0] / self.subsample_ratio
         target_y =   rect[0][1] / self.subsample_ratio
         offset_px = (target_x - self.image_center[0]) , -1.0*(target_y - self.image_center[1])
-        
+
         #NOTE!! When using a ball shaped object, use minEnclosingCircle and the circle diameter
         #enc_circle = 2 * cv2.minEnclosingCircle(c)[1]
         #height_px = 2 * enc_circle[1]
@@ -119,56 +121,63 @@ class DistanceTracker:
     def set_rectified(self, rectified):
         self.rectified = rectified
 
-    def run(self, ros_data):
+    def run(self):
         """Run the Distance Tracker with the input from the camera"""
         bridge = CvBridge()
-        cv_image = None
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            dist, offset = self.find_position(self.focal_length, self.real_height)
+
+            print("EST DISTANCE: " + str(dist) + ' inches')
+            print("EST OFFSET: " + str(offset) + ' inches')
+            print("--------------")
+
+            #heading averaging
+            self.last_point = self.current_point
+            self.current_point = offset[0]
+            new_slope = (self.current_point - self.last_point)
+            if new_slope != 0:
+                new_slope = new_slope / abs(new_slope)
+            if self.current_slope == 1:
+                if new_slope < 0:
+                    self.average = self.current_sum / self.count
+                    self.current_sum = 0
+                    self.count = 0
+            self.current_sum += self.current_point
+            self.count += 1
+            self.current_slope = new_slope
+
+            #publish the heading and the average heading
+            self.heading_pub.publish(offset[0])
+            self.avg_pub.publish(self.average)
+
+            #put a red circle on the image where the average heading is
+            if dist != 0:
+                center_x = int(self.average*self.focal_length/dist + self.image_center[0])
+                center_y = int(-1.0*offset[1]*self.focal_length/dist + self.image_center[1])
+                cv_marked_image = cv2.circle(self.image, (center_x, center_y), 15, (0,0,255), 15)
+                marked_image = bridge.cv2_to_imgmsg(cv_marked_image, encoding='bgr8')
+                self.image_pub.publish(marked_image)
+            rate.sleep()
+
+    def callback(self, ros_data):
+        bridge = CvBridge()
         if self.rectified:
-            cv_image = bridge.imgmsg_to_cv2(ros_data, desired_encoding='passthrough')
+            self.image = bridge.imgmsg_to_cv2(ros_data, desired_encoding='passthrough')
         else:
-            cv_image = bridge.compressed_imgmsg_to_cv2(ros_data, desired_encoding='passthrough')
-        
-        dist, offset = self.find_position(self.focal_length, self.real_height, cv_image)
-        
-        print("EST DISTANCE: " + str(dist) + ' inches')
-        print("EST OFFSET: " + str(offset) + ' inches')
-        print("--------------")
+            self.image = bridge.compressed_imgmsg_to_cv2(ros_data, desired_encoding='passthrough')
 
-        #heading averaging
-        self.last_point = self.current_point
-        self.current_point = offset[0]
-        new_slope = (self.current_point - self.last_point)
-        if new_slope != 0:
-            new_slope = new_slope / abs(new_slope)
-        if self.current_slope == 1:
-            if new_slope <= 0:
-                self.average = self.current_sum / self.count
-                self.current_sum = 0
-                self.count = 0
-        self.current_sum += self.current_point
-        self.count += 1
-        self.current_slope = new_slope
-
-        #publish the heading and the average heading
-        self.heading_pub.publish(offset[0])
-        self.avg_pub.publish(self.average)
-
-        #put a red circle on the image where the average heading is
-        if dist != 0:
-            center_x = int(self.average*self.focal_length/dist + self.image_center[0])
-            center_y = int(-1.0*offset[1]*self.focal_length/dist + self.image_center[1])
-            cv_marked_image = cv2.circle(cv_image, (center_x, center_y), 15, (0,0,255), 15)
-            marked_image = bridge.cv2_to_imgmsg(cv_marked_image, encoding='bgr8')
-            self.image_pub.publish(marked_image)
-        
 
 if __name__ == '__main__':
     rospy.init_node('dist_tracker', anonymous=True)
-    tracker = DistanceTracker()    
+    tracker = DistanceTracker()
     if (len(sys.argv) > 1):
         if ("--rectified" in sys.argv):
             tracker.set_rectified(True)
-            rospy.Subscriber('/raspicam_node/image_rect_color', Image, tracker.run)
+            rospy.Subscriber('/raspicam_node/image_rect_color', Image, tracker.callback)
+            print("Beginning position tracker with rectified image at 10hz\n")
     else:
-        rospy.Subscriber('/raspicam_node/image/compressed', CompressedImage, tracker.run)
-    rospy.spin()
+        rospy.Subscriber('/raspicam_node/image/compressed', CompressedImage, tracker.callback)
+        print("Beginning position tracker at 10hz\n")
+    tracker.run()
+    print("Done\n")
